0d2a1bc56cd6ba29349038f41711aca49e4aece0
Burdeina Tetiana <tania545556@gmail.com>
2020-04-24 23:00:07 +0300

    Invert values

---
+++ 8kyu/Invert values.js
@@ -0,0 +1,9 @@
+//https://www.codewars.com/kata/5899dc03bc95b1bf1b0000ad
+//Invert values
+
+function invert(array) {
+    for (let i = 0; i < array.length; i++){
+        array[i] = -array[i];
+    }
+    return array;
+}
