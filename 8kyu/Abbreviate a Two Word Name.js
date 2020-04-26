352fe1cf9b0f6608f123fdcf3abe4e51e1f3fc02
Burdeina Tetiana <tania545556@gmail.com>
2020-04-26 23:34:19 +0300

    Leap Years

--- 8kyu/Abbreviate a Two Word Name.js
+++ 8kyu/Abbreviate a Two Word Name.js
@@ -1,9 +1,9 @@
-//https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3
+//https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3ы
 //Abbreviate a Two Word Name
 
-function abbrevName(n){
+function abbrevName(n) {
     let str = '';
-    for (let i = 0; i < n.length; i++){
+    for (let i = 0; i < n.length; i++) {
         if (n[i] === ' ') str += n[0].toUpperCase() + '.' + n[i + 1].toUpperCase();
     }
     return str;
