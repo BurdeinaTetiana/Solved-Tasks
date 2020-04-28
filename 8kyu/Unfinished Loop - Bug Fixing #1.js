cb917cc143de5405760d5a040a62d88cf66b251d
Burdeina Tetiana <tania545556@gmail.com>
2020-04-28 23:08:14 +0300

    Unfinished Loop - Bug Fixing #1

---
+++ 8kyu/Unfinished Loop - Bug Fixing #1.js
@@ -0,0 +1,12 @@
+//https://www.codewars.com/kata/55c28f7304e3eaebef0000da
+//Unfinished Loop - Bug Fixing #1
+
+function createArray(number){
+    let newArray = [];
+
+    for(let i = 1; i <= number; i++){
+        newArray.push(i);
+    }
+
+    return newArray;
+}
