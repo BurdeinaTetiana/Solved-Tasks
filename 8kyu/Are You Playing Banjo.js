0a21325e4cd12f71c6d16cceb9b968858d31e491
Burdeina Tetiana <tania545556@gmail.com>
2020-05-01 23:53:11 +0300

    Are You Playing Banjo?

---
+++ 8kyu/Are You Playing Banjo.js
@@ -0,0 +1,8 @@
+//https://www.codewars.com/kata/53af2b8861023f1d88000832
+
+//Are You Playing Banjo?
+
+function areYouPlayingBanjo(n) {
+    if (n[0] === 'r' || n[0] === 'R') return n + " plays banjo"
+    else return n + " does not play banjo";
+}
