569550669fe99b83bfefafcc6c6b5b5c44adaf4b
Burdeina Tetiana <tania545556@gmail.com>
2020-05-15 23:02:57 +0300

    Count the Monkeys!

---
+++ 8kyu/Count the Monkeys!.js
@@ -0,0 +1,11 @@
+//https://www.codewars.com/kata/56f69d9f9400f508fb000ba7
+
+//Count the Monkeys!
+
+function monkeyCount(n) {
+    const arr = [];
+    for( let i = 1; i <=n; i++){
+        arr.push(i);
+    }
+    return arr;
+}
